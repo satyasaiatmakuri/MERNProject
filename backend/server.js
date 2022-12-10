e52306af8e5e0c20ea9cb377ead330a44a8a8e82
satyasaiatmakuri <78465461+satyasaiatmakuri@users.noreply.github.com>
2022-12-10 16:32:21 +0530

    error handling

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,28 @@
-const app =require('./app');
-const connectDatabase = require('./config/database');
+const app = require("./app");
+const connectDatabase = require("./config/database");
 
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 
-dotenv.config({path: 'backend/config/config.env'})
+process.on("uncaughtException", (err) => {
+  console.log(`ERROR: ${err.message}`);
+  console.log(`Shutting down server due to uncaught exception`);
+  process.exit(1);
+});
+
+dotenv.config({ path: "backend/config/config.env" });
 
 connectDatabase();
 
-app.listen(process.env.PORT, ()=>{
-    console.log(`Server start in ${process.env.PORT} in ${process.env.NODE_ENV} mode.`)
-})
+const server = app.listen(process.env.PORT, () => {
+  console.log(
+    `Server start in ${process.env.PORT} in ${process.env.NODE_ENV} mode.`
+  );
+});
+
+process.on("unhandledRejection", (err) => {
+  console.log(`ERROR: ${err.message}`);
+  console.log(`Shutting down the server due to unhandled promice rejection`);
+  server.close(() => {
+    process.exit(1);
+  });
+});
